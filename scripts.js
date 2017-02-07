ec3964c4b6ad86511166b486b58b72f4b8861241
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-02-06 22:50:36 -0700

    created sketchy way to get divs created by button click and populate divs with info.

--- scripts.js
+++ scripts.js
@@ -15,11 +15,36 @@
 //on click, create new div element with .HTML.
 //within each element, append the div to store name and website
 
+// $('#enter-button').on('click', function () {
+//   var pressCounter = 0;
+//   pressCounter += 1;
+//   var webTitle = $('#web-title').val();
+//   var webAddress = $('#web-address').val();
+//   $('.bookmarks').attr('id', 1);
+//   $('#'+ 1).append("<h2>" + webTitle + "</h2>");
+//   $('#'+ 1).append("<a>" + webAddress + "</a>");
+//   // $('#title').text(webTitle);
+//   // $('#address').text(webAddress);
+//   console.log(webTitle);
+// });
+
+//---Creates a DIV and adds data-----//
 $('#enter-button').on('click', function () {
+  var num = 0;
+  var pressCounter;
+  var newEntry;
+  num += 1;
+  pressCounter = num.toString();
+  newEntry = "count" + pressCounter;
+  console.log(newEntry);
   var webTitle = $('#web-title').val();
   var webAddress = $('#web-address').val();
-  $('#display-side').append()
-  $('#title').text(webTitle);
-  $('#address').text(webAddress);
+  $('#display-side').append("<div class ='bookmarks' id ='" + newEntry +"'></div>");
+  $('#'+ newEntry).append("<h2>" + webTitle + "</h2>");
+  $('#'+ newEntry).append("<hr>");
+  $('#'+ newEntry).append("<a>" + webAddress + "</a>");
+  $('#'+ newEntry).append("<hr>");
+  $('#'+ newEntry).append("<button class='read'>Read</button>");
+  $('#'+ newEntry).append("<button class='delete'>Delete</button>");
   console.log(webTitle);
 });
