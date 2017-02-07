c215bb2d9b27cc71665aba5791ccfe97f7cd252f
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-02-06 21:46:14 -0700

    changed css styling, created button click function, modified html slightly.

--- scripts.js
+++ scripts.js
@@ -11,3 +11,15 @@
 // if (bookmark field is full ){
 //   then fill in next field
 // }
+
+//on click, create new div element with .HTML.
+//within each element, append the div to store name and website
+
+$('#enter-button').on('click', function () {
+  var webTitle = $('#web-title').val();
+  var webAddress = $('#web-address').val();
+  $('#display-side').append()
+  $('#title').text(webTitle);
+  $('#address').text(webAddress);
+  console.log(webTitle);
+});
