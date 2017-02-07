b09f97e0a2cdf204ccea59c7a24b23a0d0231cf5
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-02-07 16:16:33 -0700

    refactoring if statements.

--- scripts.js
+++ scripts.js
@@ -7,11 +7,6 @@
 //.  use jquery toggleClass to toggle .read
 //.
 //3.
-
-// if (bookmark field is full ){
-//   then fill in next field
-// }
-
 //on click, create new div element with .HTML.
 //within each element, append the div to store name and website
 
@@ -41,12 +36,24 @@ function submitData (){
   } else if (noHttp === false){
     var address = "http://" + webAddress;
   }
-
-
   console.log(webTitle + webAddress);
 
   //Then append with div + data
   $('#display-side').append("<div class='bookmarks'><h2>" + webTitle + "</h2><hr><a href='"+ address +"'>" + address + "</a><hr><button class='read'>Read</button><button class='delete'>Delete</button></div>");
+}
+
+function evalAddress() {
+  if (noWww === false && noHttp === false){
+    var address = "http://" + "www." + webAddress;
+  } else if (noWww === true && noHttp === true){
+    var address = webAddress;
+  } else if (noWww === false){
+    var address = "www." + webAddress;
+    var address = webAddress.replace("http://", "http://www.")
+  } else if (noHttp === false){
+    var address = "http://" + webAddress;
+  }
+  return address
 }
 
 
