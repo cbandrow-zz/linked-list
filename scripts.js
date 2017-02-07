688db7c541fe35abf725640f3df626966ea35a3e
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-02-06 17:36:45 -0700

    added jquery, added JS steps, added normalize css.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Javascript Functionality
+
+//1. We have 2 input
+//  Connect each input to the the Enter button
+//  Use jQuery to connect input to button and button to bookmark fields
+//2. Click Read button
+//.  use jquery toggleClass to toggle .read
+//.
+//3.
+
+// if (bookmark field is full ){
+//   then fill in next field
+// }
