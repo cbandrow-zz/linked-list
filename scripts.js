7b3d40bf025f33a5c5100e4552c6b4597812e87b
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-07 00:15:03 -0700

    Add jp-notes.md, Add bookmark styles, Add read button toggleClass

--- scripts.js
+++ scripts.js
@@ -11,3 +11,29 @@
 // if (bookmark field is full ){
 //   then fill in next field
 // }
+
+
+//Code for Read buttons
+$('#read-button-1').on('click', function () {
+  $('#read-button-1').parent().toggleClass('read-bookmark');
+  $('#a-1').toggleClass('read-link');
+  $('#read-button-1').toggleClass('read-button');
+});
+
+$('#read-button-2').on('click', function () {
+  $('#read-button-2').parent().toggleClass('read-bookmark');
+  $('#a-2').toggleClass('read-link');
+  $('#read-button-2').toggleClass('read-button');
+});
+
+$('#read-button-3').on('click', function () {
+  $('#read-button-3').parent().toggleClass('read-bookmark');
+  $('#a-3').toggleClass('read-link');
+  $('#read-button-3').toggleClass('read-button');
+});
+
+$('#read-button-4').on('click', function () {
+  $('#read-button-4').parent().toggleClass('read-bookmark');
+  $('#a-4').toggleClass('read-link');
+  $('#read-button-4').toggleClass('read-button');
+});
