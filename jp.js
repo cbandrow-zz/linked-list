f5d915736ef46cb245218b3298a3e09884f44139
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-08 15:02:48 -0700

    Add bookmark count text, Add Clear Bookmark button, Add Count Bookmark function, Remove uneccessary comments

--- jp.js
+++ jp.js
@@ -1,3 +1,9 @@
+function bookmarkCount() {
+var $bookmarkCount = $('.bookmarks');
+console.log($bookmarkCount.length);
+$('#bookmark-count').text($bookmarkCount.length);
+}
+
 $('#enter-button').on('click',function() {
   var $webTitle = $('#web-title').val();
   var $webAddress = $('#web-address').val();
@@ -5,6 +11,8 @@ $('#enter-button').on('click',function() {
   $webAddress = $webAddress.includes("http://") ? $webAddress :  $webAddress.includes("www") ? "http://" + $webAddress : "http://" + "www." + $webAddress;
 
   $('#display-side').prepend("<div class='bookmarks'><h2>" + $webTitle + "</h2><hr><a class='link' target='_blank' href='" + $webAddress + "'>" + $webAddress + "</a><hr><button class='read'>Read</button><button class='delete'>Delete</button></div>");
+
+  bookmarkCount();
 
   $('#web-title').val('');
   $('#web-address').val('');
@@ -16,16 +24,19 @@ $('#display-side').on('click','.read', function(event) {
   $(this).closest('.bookmarks').toggleClass('read-bookmark');
   $(this).closest('.link').toggleClass('read-link');
   $(this).toggleClass('read-button');
-  // $('.read').toggleClass('read-button');
-  // $('.bookmarks').toggleClass('read-bookmark');
-  // $('.link').toggleClass('read-link');
 
 
-  var $readButtonCount = $('.read');
-  console.log($readButtonCount);
 
 });
 
-$('#display-side').on('click','.delete', function() {
+$('#display-side').on('click','.delete', function(event) {
   console.log("Dum diddy dum diddy delete");
+  $(this).closest('.bookmarks').remove();
+  bookmarkCount();
+});
+
+$('#clear-button').on('click', function(event) {
+  console.log("CLEAR!!!");
+  $('.bookmarks').remove();
+  bookmarkCount();
 });
