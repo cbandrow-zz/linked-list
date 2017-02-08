00fc53a843d5c4760cac60905866f3a9ff42161d
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-08 02:49:20 -0700

    Add $ to jQuery variables, clear input fields on submit

--- jp.js
+++ jp.js
@@ -4,12 +4,14 @@ $('#enter-button').on('click',function() {
 
 function submitData() {
 
-  var webTitle = $('#web-title').val();
-  var webAddress = $('#web-address').val();
+  var $webTitle = $('#web-title').val();
+  var $webAddress = $('#web-address').val();
 
-  address = webAddress.includes("http://") ? webAddress :  webAddress.includes("www") ? "http://" + webAddress : "http://" + "www." + webAddress;
+  $webAddress = $webAddress.includes("http://") ? $webAddress :  $webAddress.includes("www") ? "http://" + $webAddress : "http://" + "www." + $webAddress;
 
-  $('#display-side').append("<div class='bookmarks'><h2>" + webTitle + "</h2><hr><a target='_blank' href='" + address + "'>" + address + "</a><hr><button class='read'>Read</button><button class='delete'>Delete</button></div>");
+  $('#display-side').prepend("<div class='bookmarks'><h2>" + $webTitle + "</h2><hr><a target='_blank' href='" + $webAddress + "'>" + $webAddress + "</a><hr><button class='read'>Read</button><button class='delete'>Delete</button></div>");
 
-  console.log(webAddress + " " + address);
+  $('#web-title').val('');
+  $('#web-address').val('');
+
 }
