657b56442b1d3549077c5236d66dfa45abdfa10a
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-09 02:51:27 -0700

    add dissable state to clear button

--- scripts.js
+++ scripts.js
@@ -1,30 +1,17 @@
-$("#web-title, #web-address").on("keyup", disable);
+$("#web-title, #web-address").on("keyup", disableEnter);
 
 $('#enter-button').on('click',function() {
   var $webTitle = $('#web-title').val();
   var $webAddress = $('#web-address').val();
-  if ($webTitle ==="" || $webAddress===""){
-    $('#non-entry').text("Try entering a valid site and URL");
-  }
-  else {
-    addData($webAddress, $webTitle);
-  }
+  $webAddress = $webAddress.includes("http://") ? $webAddress :  $webAddress.includes("www") ? "http://" + $webAddress : "http://" + "www." + $webAddress;
+  $('#display-side').prepend("<div class='bookmarks'><h2>" + $webTitle + "</h2><hr><a class='link' target='_blank' href='" + $webAddress + "'>" + $webAddress + "</a><hr><button class='read'>Read</button><button class='delete'>Delete</button></div>");
   $('#web-title').val('');
   $('#web-address').val('');
-  disable();
-});
-
-
-function addData($webAddress, $webTitle){
-  $webAddress = $webAddress.includes("http://") ? $webAddress :  $webAddress.includes("www") ? "http://" + $webAddress : "http://" + "www." + $webAddress;
-
-  $('#display-side').prepend("<div class='bookmarks'><h2>" + $webTitle + "</h2><hr><a class='link' target='_blank' href='" + $webAddress + "'>" + $webAddress + "</a><hr><button class='read'>Read</button><button class='delete'>Delete</button></div>");
-
   bookmarkCount();
-  $('#non-entry').text("");
-}
+  disableEnter();
+  disableClearBookmarks();
 
-
+});
 
 $('#display-side').on('click','.read', function(event) {
   $(this).closest('.bookmarks').toggleClass('read-bookmark');
@@ -32,23 +19,31 @@ $('#display-side').on('click','.read', function(event) {
   $(this).toggleClass('read-button');
 
 });
-
 $('#display-side').on('click','.delete', function(event) {
   $(this).closest('.bookmarks').remove();
   bookmarkCount();
+  disableClearBookmarks();
 });
-
 $('#clear-button').on('click', function(event) {
   $('.bookmarks').remove();
-  disable();
+  disableEnter();
   bookmarkCount();
 });
 
-function disable(){
+function disableEnter(){
   if($("#web-title").val().length > 0 && $("#web-address").val().length > 0) {
     $("#enter-button").prop("disabled", false);
   } else {
     $("#enter-button").prop("disabled", true);
+  }
+}
+
+function disableClearBookmarks() {
+  if ($('.bookmarks').length < 1) {
+    $("#clear-button").prop("disabled", true);
+  }
+  else {
+    $("#clear-button").prop("disabled", false);
   }
 }
 
